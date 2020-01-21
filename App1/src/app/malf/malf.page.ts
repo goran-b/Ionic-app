7408307fada67c614998bba4d20e2d1c3957cb74
goran-b <goran.b@mail.com>
2020-01-21 17:58:48 +0100

    add better ui

--- App1/src/app/malf/malf.page.ts
+++ App1/src/app/malf/malf.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MalfService } from './malf.service';
+import { Malf } from '../models/malf.model';
 
 @Component({
   selector: 'app-malf',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./malf.page.scss'],
 })
 export class MalfPage implements OnInit {
-
-  constructor() { }
+  malfs: Malf[]
+  todayMalfs = []
+  listMalfs = []
+  constructor(private malfService: MalfService) { }
 
   ngOnInit() {
+    this.malfService.getUserMalfs().subscribe((r) => {
+      this.malfs = r.map(a => {
+        const data = a.payload.doc.data() as Malf;
+        data.id = a.payload.doc.id;
+        return data
+      });
+      let todayDay = new Date().setHours(0, 0, 0, 0)
+      this.todayMalfs = Object.values(this.malfs);
+      this.listMalfs = Object.values(this.malfs);
+      this.todayMalfs = this.todayMalfs.filter((m) => parseFloat(m.date.toString()) >= todayDay)
+      this.listMalfs = this.listMalfs.filter((m) => parseFloat(m.date.toString()) < todayDay)
+    })
   }
-
 }
